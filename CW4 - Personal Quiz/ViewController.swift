aadf6247ba091e03affef83245ffa94f71e3a796
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-09-30 13:34:17 +0300

    Initial Commit

---
+++ CW4 - Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CW4 - Personal Quiz
+//
+//  Created by Сергей Ткаченко on 30.09.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
