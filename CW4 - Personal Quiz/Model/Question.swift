84af15fd2fc28d52234e527fa30f2b5e8b405deb
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-01 13:31:51 +0300

    Instal UI

---
+++ CW4 - Personal Quiz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  CW4 - Personal Quiz
+//
+//  Created by Сергей Ткаченко on 01.10.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var ansvers: [String]
+}
---
+++ CW4 - Personal Quiz/Model/ResponseType.swift
@@ -0,0 +1,14 @@
+//
+//  ResponseType.swift
+//  CW4 - Personal Quiz
+//
+//  Created by Сергей Ткаченко on 01.10.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
