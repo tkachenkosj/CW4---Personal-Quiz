3b57283393181e5295376e9ae8be7c0c53b7ffa0
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-01 13:58:05 +0300

    Create models

---
+++ CW4 - Personal Quiz/Model/Answer.swift
@@ -0,0 +1,11 @@
+//
+//  Answer.swift
+//  CW4 - Personal Quiz
+//
+//  Created by Сергей Ткаченко on 01.10.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
--- CW4 - Personal Quiz/Model/Question.swift
+++ CW4 - Personal Quiz/Model/Question.swift
@@ -9,5 +9,42 @@
 struct Question {
     var text: String
     var type: ResponseType
-    var ansvers: [String]
+    var ansvers: [Answer]
+}
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        return [
+            Question(
+                text: "Какую пищу вы предпочитаете?",
+                type: .single,
+                ansvers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtile)
+                ]
+            ),
+            Question(
+                text: "Что вам больше нравится?",
+                type: .multiple,
+                ansvers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rabbit),
+                    Answer(text: "Есть", type: .turtile)
+                ]
+            ),
+            Question(
+                text: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                ansvers: [
+                    Answer(text: "Ненавижу", type: .dog),
+                    Answer(text: "Нервничаю", type: .cat),
+                    Answer(text: "Не замечаю", type: .rabbit),
+                    Answer(text: "Обожаю", type: .turtile)
+                ]
+            ),
+        ]
+    }
 }
